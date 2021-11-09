e002b38780dd7dfe8fb9a05296f17087f299d276
MarcoGranieri <marcogranieri@libero.it>
2021-11-09 17:12:15 +0100

    completed issue-tracker project

---
+++ issue-tracker/src/app/issue-report/issue-report.component.ts
@@ -0,0 +1,35 @@
+import { IssuesService } from './../issues.service';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-issue-report',
+  templateUrl: './issue-report.component.html',
+  styleUrls: ['./issue-report.component.css']
+})
+export class IssueReportComponent implements OnInit {
+  issueForm: FormGroup | undefined;
+  @Output() formClose = new EventEmitter();
+
+  constructor(private builder: FormBuilder,
+     private issueService: IssuesService) { }
+
+  ngOnInit(): void {
+    this.issueForm = this.builder.group({
+      title: ['', Validators.required],
+      description: [''],
+      priority: ['', Validators.required],
+      type: ['', Validators.required]
+      });
+  }
+
+  addIssue() {
+    if (this.issueForm && this.issueForm.invalid) {
+    this.issueForm.markAllAsTouched();
+    return;
+    }
+
+    this.issueService.createIssue(this.issueForm?.value);
+    this.formClose.emit();
+    }
+}
